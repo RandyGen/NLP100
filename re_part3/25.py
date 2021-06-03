649fe4a1b1cd78b54594ddeb0a195d10942b84ee
RandyGen <sota.nemoto.5s@stu.hosei.ac.jp>
2021-06-03 20:43:28 +0900

    finished retry part3

--- re_part3/25.py
+++ re_part3/25.py
@@ -1,6 +1,7 @@
 # unsolved
 import json
 import re
+import pprint
 
 path = 'eu.json'
 
@@ -10,12 +11,16 @@ with open(path, 'r') as f:
     for line in f:
         data += line
 
+# .*? .* の違い
+# re.DOTALL $ の使い方
 
-pattern = r"^.*\{\{基礎情報(.*)\}\}.*$"
-info = re.findall(pattern, data, re.MULTILINE)
+pattern = r"^.*\{\{基礎情報.*?$(.*)^\}\}.*"
+info = re.findall(pattern, data, re.MULTILINE + re.DOTALL)
 
-print(info)
+# print(info)
 
-pattern = r"^\|(.+?)(?:\s=\s*.*)$"
-result = '\n'.join(re.findall(pattern, data))
-print(result)
+
+# \s \s+ の違い
+pattern = r"^\|(.+?)\s*=\s*(.+?)\n"
+result = dict(re.findall(pattern, data, re.MULTILINE + re.DOTALL))
+pprint.pprint(result)
